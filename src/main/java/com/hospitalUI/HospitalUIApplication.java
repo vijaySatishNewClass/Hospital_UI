5058d6dcc85fe852d95a481bf8a0deb65fe51d32
vijay <vijay.rathore@ekaplus.com>
2019-12-19 12:57:23 +0530

    changes  made for main method for command liner

--- src/main/java/com/hospitalUI/HospitalUIApplication.java
+++ src/main/java/com/hospitalUI/HospitalUIApplication.java
@@ -1,13 +1,34 @@
 package com.hospitalUI;
 
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HospitalUIApplication {
+public class HospitalUIApplication {// implements CommandLineRunner{
+
+	// @Autowired
+	// private ApplicationContext appContext;
+
+	// public static void main(String[] args) {
+	// SpringApplication app = new SpringApplication(HospitalUIApplication.class);
+	// app.setBannerMode(Mode.OFF);
+	// app.run(args);
+	// SpringApplication.run(HospitalUIApplication.class, args); //default
+	// }
+
+	// @Override
+	// public void run(String[] args) throws Exception {
+
+	// String[] beans = appContext.getBeanDefinitionNames();
+	// for (String bean : beans) {
+	// System.out.println(bean);
+	// }
+
+	// }
 
 	public static void main(String[] args) {
-		SpringApplication.run(HospitalUIApplication.class, args);
+		SpringApplication.run(HospitalUIApplication.class, args); // default
 	}
 
 }
--- src/main/java/com/hospitalUI/controller/AdminController.java
+++ src/main/java/com/hospitalUI/controller/AdminController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin")
+//@RequestMapping("/admin")
 public class AdminController {
 
 	private AdminService adminService;
